50cd6a2cc3976d7f27d38b5869b7eb5e41d0c5ec
Jhon Parra <jhonparra939@gmail.com>
2023-08-26 16:01:52 -0500

    llm test updated

--- test/test_llm.py
+++ test/test_llm.py
@@ -1,9 +1,10 @@
 from text_summarizer import agent
+import os
 
-test_img="test/test_img.jpg"
+test_img="test_img.jpg"
 
 class TestLLM:
     def test_llm(self):
         user_question = "Hello, could you please tell me what products were bought in this receipt i'm attaching? return a python dictionary with all you find"
-        response = agent.run(f'{user_question}, here is the image path: {test_img}')
+        response = agent.run(f'{user_question}, here is the image path: {os.path.join(os.path.dirname(__file__), test_img)}')
         assert "tequila" in response.lower(),ValueError("Tequila not found in the receipt")
